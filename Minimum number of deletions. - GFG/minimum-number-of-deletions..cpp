3c591500e3ae962302f0efb0d879132da8733671
Sumit Kumar <93362896+Rootsrobut@users.noreply.github.com>
2023-10-18 10:55:33 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int minDeletions(string str, int n);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        cout << minDeletions(s, n) << endl;
+
+    }
+return 0;
+}
+// } Driver Code Ends
+
+int lcs(int n, int m, string str1, string str2) {
+        int t[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                t[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        return t[n][m];
+    }
+int minDeletions(string str, int n) { 
+     string str1 = str; 
+        reverse(str1.begin(), str1.end());
+        int m = str1.length(); 
+        int ans = lcs(n, m, str, str1); 
+        return n-ans;
+} 
